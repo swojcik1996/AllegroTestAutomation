9036bb7c6976344be1cbfcd79483bdb9ba899147
swojcik1 <slawek1lksm080>
2019-02-19 12:43:59 +0100

    productPage & clean code

---
+++ src/main/java/pages/ProductPage.java
@@ -0,0 +1,37 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class ProductPage extends BasePage {
+    @FindBy(xpath = "//h1[@class='fb442b3f']")
+    private WebElement name;
+
+    @FindBy(xpath = "//div[@class='_7c5a1027 _578d07cf cf7a65ff']")
+    private WebElement price;
+
+    @FindBy(xpath = "//div[@class='_3def0f7a ad311378']//input")
+    private WebElement quantity;
+
+    @FindBy(xpath = "//button[@id='add-to-cart-button']")
+    private WebElement addTobasket;
+
+    @FindBy(id = "add-to-cart-si-precart")
+    private WebElement dialogGoToBasket;
+
+    public ProductPage(WebDriver driver) {
+        super(driver);
+        PageFactory.initElements(driver,this);
+    }
+
+    public ProductPage addToBasket(){
+        System.out.println(name.getText());
+        System.out.println(price.getText());
+        System.out.println(quantity.getAttribute("value"));
+
+        addTobasket.click();
+        return this;
+    }
+}
--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -4,20 +4,41 @@
  */
 import models.BaseTest;
 import models.Order;
+import models.Product;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import pages.ProductListPage;
+import pages.ProductPage;
 import pages.menu.MenuPage;
 import pages.RegulationsPage;
 
+import java.math.BigDecimal;
+
 public class Main extends BaseTest {
+    private Order order;
+    private RegulationsPage regulationsPage;
+    private MenuPage menuPage;
+    private ProductListPage productListPage;
+    private ProductPage productPage;
+
+    @BeforeEach
+    public void checkoutSetup(){
+        order = new Order();
+        regulationsPage = new RegulationsPage(driver);
+        menuPage = new MenuPage(driver);
+        productListPage = new ProductListPage(driver);
+        productPage = new ProductPage(driver);
+    }
 
     @Test
     public void checkoutTest(){
-        Order order = new Order();
+        regulationsPage.accept();
 
-        new RegulationsPage(driver).accept();
-        new MenuPage(driver).chooseRandomCategory();
+        menuPage.chooseRandomCategory();
+        productListPage.randProductPage().randProduct();
+        productPage.addToBasket();
 
-        new ProductListPage(driver).randProductPage().randProduct();
+        Product product = new Product("test",new BigDecimal(22), 2,new BigDecimal(44));
     }
 }
